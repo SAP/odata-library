58bb17bcef8dc74a2e11ff73d5e3b13041c55b4d
Norbert Volf <norbert.volf@sap.com>
2021-03-16 21:26:00 +0100

    [FIX] Parse PropertyPath with Navigation Property
    Annotations could define property as path with
    link to property from navigation property property
    instead of link to own property. The patch implements
    finding of the property in navigation property.
    
    Topic: #20

--- test/unit/model/nw/extensions/sap/common/SideEffectsType.js
+++ test/unit/model/nw/extensions/sap/common/SideEffectsType.js
@@ -1,34 +1,42 @@
 "use strict";
 
-const assert = require("assert");
+const assert = require("assert").strict;
 const SideEffectsType = require("../../../../../../../lib/model/nw/extensions/sap/common/SideEffectsType");
-
-let annotation = {
-  record: {
-    value: {
-      SourceEntities: {
-        collection: ["a"],
-      },
-      SourceProperties: {
-        collection: ["a"],
-      },
-      TargetEntities: {
-        collection: ["a", "b"],
-      },
-      TargetProperties: {
-        collection: ["a", "b"],
-      },
-    },
-  },
-};
-
-let entityType = {
-  getNavigationProperty: () => "navProp",
-  getProperty: () => "prop",
-};
+const sinon = require("sinon");
+const sandbox = sinon.createSandbox();
 
 describe("SideEffectsType", function () {
+  afterEach(function () {
+    sandbox.restore();
+  });
   describe("#constructor()", function () {
+    let entityType;
+    let annotation = {
+      record: {
+        value: {
+          SourceEntities: {
+            collection: ["a"],
+          },
+          SourceProperties: {
+            collection: ["a"],
+          },
+          TargetEntities: {
+            collection: ["a", "b"],
+          },
+          TargetProperties: {
+            collection: ["a", "b"],
+          },
+        },
+      },
+    };
+
+    beforeEach(function () {
+      entityType = {
+        getNavigationProperty: () => "navProp",
+        getProperty: () => "prop",
+      };
+    });
+
     it("initializes properties", function () {
       let sideEffects = new SideEffectsType(annotation, entityType);
 
@@ -48,6 +56,43 @@ describe("SideEffectsType", function () {
       assert.strictEqual(sideEffects.sourceProperties[0], "prop");
       assert.strictEqual(sideEffects.targetEntities[0], "navProp");
       assert.strictEqual(sideEffects.targetProperties[0], "prop");
+    });
+
+    it("define target property as path", function () {
+      let sideEffects;
+      let transformer;
+      let target = {
+        entityType: {
+          getProperty: sinon
+            .stub()
+            .returns("PROPERTY_FROM_NAVIGATION_PROPERTY"),
+        },
+      };
+      let navigationProperty = {
+        getTarget: sinon.stub().returns(target),
+      };
+
+      sandbox.stub(SideEffectsType._, "definePropertyCollection");
+      sideEffects = new SideEffectsType(annotation, entityType, "SCHEMA");
+      assert.ok(
+        SideEffectsType._.definePropertyCollection
+          .getCall(3)
+          .calledWith(sideEffects, "TargetProperties")
+      );
+      transformer = SideEffectsType._.definePropertyCollection.getCall(3)
+        .args[2];
+
+      entityType.getProperty = sinon.stub().returns("PROPERTY");
+      entityType.getNavigationProperty = sinon
+        .stub()
+        .returns(navigationProperty);
+
+      assert.equal(
+        transformer("navigationPropertyName/propertyName"),
+        "PROPERTY_FROM_NAVIGATION_PROPERTY"
+      );
+      assert.ok(navigationProperty.getTarget.calledWithExactly("SCHEMA"));
+      assert.ok(entityType.getProperty.notCalled);
     });
   });
 });

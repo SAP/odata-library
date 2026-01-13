42ee99075286143ca8be144f0d49bdbb775fd2aa
Norbert Volf <norbert.volf@sap.com>
2026-01-13 11:16:35 +0100

    [FEATURE] Add Support for Bound Action with Type
    This commit introduces support for a bound Action that can
    include a parameter named _it. This parameter defines the
    EntityType used by the endpoint to determine the output.
    
    Additionally, one action import can be defined for multiple
    EntityType(s) that share the same name. In such cases, the
    EntityType specifies the key based on metadata annotations.

--- test/unit/model/oasis/CsdlSchema.js
+++ test/unit/model/oasis/CsdlSchema.js
@@ -4,6 +4,8 @@ const _ = require("lodash");
 const assert = require("assert");
 const sinon = require("sinon");
 const CsdlSchema = require("../../../../lib/model/oasis/CsdlSchema");
+const Action = require("../../../../lib/model/oasis/schema/Action");
+const EntityContainer = require("../../../../lib/model/oasis/dataSource/EntityContainer");
 
 const settings = {
   strict: true,
@@ -14,76 +16,83 @@ const emptySchemaMD = {
     Namespace: "ns",
   },
 };
+let sampleSchemaMD;
 
-const sampleSchemaMD = {
-  $: {
-    Namespace: "ns.a",
-  },
-  EntityType: [
-    {
-      $: {
-        Name: "EntityType1",
-      },
-      Key: [
-        {
-          PropertyRef: [],
-        },
-      ],
-    },
-  ],
-  ComplexType: [
-    {
-      $: {
-        Name: "ComplexType1",
-      },
-    },
-  ],
-  EnumType: [
-    {
-      $: {
-        Name: "EnumType1",
-      },
-    },
-  ],
-  EntityContainer: [
-    {
-      $: {
-        Name: "EntityContainer",
-        "m:IsDefaultEntityContainer": "true",
-      },
-      EntitySet: [
-        {
-          $: {
-            Name: "EntitySet1",
-            EntityType: "ns.a.EntityType1",
-          },
-        },
-        {
-          $: {
-            Name: "EntitySet2",
-            EntityType: "ns.a.EntityType1",
-          },
-        },
-      ],
-    },
-  ],
-  Annotations: [
-    {
-      $: {
-        Target: "ns.a.EntityContainer/EntitySet2",
-      },
-      Annotation: [
-        {
-          $: {
-            Term: "t1",
-          },
-        },
-      ],
-    },
-  ],
-};
+const sandbox = sinon.createSandbox();
 
 describe("CsdlSchema", function () {
+  beforeEach(function () {
+    sampleSchemaMD = {
+      $: {
+        Namespace: "ns.a",
+      },
+      EntityType: [
+        {
+          $: {
+            Name: "EntityType1",
+          },
+          Key: [
+            {
+              PropertyRef: [],
+            },
+          ],
+        },
+      ],
+      ComplexType: [
+        {
+          $: {
+            Name: "ComplexType1",
+          },
+        },
+      ],
+      EnumType: [
+        {
+          $: {
+            Name: "EnumType1",
+          },
+        },
+      ],
+      EntityContainer: [
+        {
+          $: {
+            Name: "EntityContainer",
+            "m:IsDefaultEntityContainer": "true",
+          },
+          EntitySet: [
+            {
+              $: {
+                Name: "EntitySet1",
+                EntityType: "ns.a.EntityType1",
+              },
+            },
+            {
+              $: {
+                Name: "EntitySet2",
+                EntityType: "ns.a.EntityType1",
+              },
+            },
+          ],
+        },
+      ],
+      Annotations: [
+        {
+          $: {
+            Target: "ns.a.EntityContainer/EntitySet2",
+          },
+          Annotation: [
+            {
+              $: {
+                Term: "t1",
+              },
+            },
+          ],
+        },
+      ],
+    };
+  });
+  afterEach(function () {
+    sandbox.restore();
+  });
   describe("#constructor()", function () {
     it("initializes properties", function () {
       let schema = new CsdlSchema(emptySchemaMD, settings);
@@ -160,6 +169,28 @@ describe("CsdlSchema", function () {
   });
 
   describe(".resolveModelPath()", function () {
+    it("resolve path for action identified by entity type", function () {
+      sampleSchemaMD.Action = [
+        {
+          $: {
+            Name: "UpdatePrices",
+            IsBound: true,
+          },
+        },
+      ];
+      sandbox.stub(Action.prototype, "matchModelPath").returns(true);
+      sandbox.stub(Action.prototype, "initSchemaDependentProperties");
+      sandbox.stub(EntityContainer.prototype, "initSchemaDependentProperties");
+      sandbox.stub(CsdlSchema.prototype, "applyAnnotations");
+
+      let schema = new CsdlSchema(sampleSchemaMD, settings);
+      assert.ok(
+        schema.resolveModelPath(
+          "ns.a.Action1(EntityType1)/SubAction"
+        ) instanceof Action
+      );
+    });
+
     it("throws on missing subelement", function () {
       let schema = new CsdlSchema(sampleSchemaMD, settings);
       schema.resolveModelPath("ns.a.EntityType1");
@@ -311,5 +342,36 @@ describe("CsdlSchema", function () {
         ]);
       });
     });
+  });
+
+  it("#parseModelPath", function () {
+    assert.deepEqual(
+      CsdlSchema.parseModelPath(
+        "ns.a.EntityContainer/EntitySet1/NavigationProperty1"
+      ),
+      {
+        path: "ns.a.EntityContainer/EntitySet1/NavigationProperty1",
+        namespace: "ns.a",
+        element: "EntityContainer",
+        subElement: "EntitySet1/NavigationProperty1",
+      }
+    );
+    assert.deepEqual(CsdlSchema.parseModelPath("ns.a.EntityContainer/"), {
+      path: "ns.a.EntityContainer/",
+      namespace: "ns.a",
+      element: "EntityContainer",
+      subElement: undefined,
+    });
+    assert.deepEqual(
+      CsdlSchema.parseModelPath(
+        "SAP__self.UpdatePrices(SAP__self.SalesOrder_Type)/PricingType"
+      ),
+      {
+        path: "SAP__self.UpdatePrices(SAP__self.SalesOrder_Type)/PricingType",
+        namespace: "SAP__self",
+        element: "UpdatePrices(SAP__self.SalesOrder_Type)",
+        subElement: "PricingType",
+      }
+    );
   });
 });
--- test/unit/model/oasis/schema/Action.js
+++ test/unit/model/oasis/schema/Action.js
@@ -4,37 +4,44 @@ const assert = require("assert");
 const sinon = require("sinon");
 const Action = require("../../../../../lib/model/oasis/schema/Action");
 const BoundObject = require("../../../../../lib/model/oasis/schema/BoundObject");
+const EntityType = require("../../../../../lib/model/oasis/schema/EntityType");
 const sandbox = sinon.createSandbox();
-
-const sampleMD = {
-  $: {
-    Name: "Action1",
-    IsBound: "true",
-    EntitySetPath: "path",
-  },
-  Parameter: [
-    {
-      $: {},
-    },
-    {
-      $: {
-        Name: "path",
-      },
-      type: "boundType",
-    },
-  ],
-  ReturnType: [
-    {
-      $: {
-        Type: "type",
-      },
-    },
-  ],
-};
 
 describe("Action", function () {
   let actionType;
+  let sampleMD;
   beforeEach(() => {
+    sampleMD = {
+      $: {
+        Name: "Action1",
+        IsBound: "true",
+        EntitySetPath: "path",
+      },
+      Parameter: [
+        {
+          $: {},
+        },
+        {
+          $: {
+            Name: "path",
+          },
+          type: "boundType",
+        },
+        {
+          $: {
+            Name: "_it",
+          },
+          type: "EntityType1",
+        },
+      ],
+      ReturnType: [
+        {
+          $: {
+            Type: "type",
+          },
+        },
+      ],
+    };
     actionType = new Action(sampleMD);
   });
   afterEach(() => {
@@ -57,5 +64,63 @@ describe("Action", function () {
       () => actionType._checkConsistency(),
       /Action Action1 may contain at most one ReturnType element/
     );
+  });
+
+  describe("matchModelPath", function () {
+    beforeEach(() => {
+      sinon.stub(actionType, "isBoundByType").returns(false);
+      actionType.isBoundByType.withArgs("EntityType1").returns(true);
+    });
+    it("match simple action path", function () {
+      assert.equal(actionType.matchModelPath({ element: "Action1" }), true);
+    });
+    it("match typed action path", function () {
+      assert.equal(
+        actionType.matchModelPath({ element: "Action1(EntityType1)" }),
+        true
+      );
+    });
+    it("missing type for typed action path", function () {
+      assert.equal(
+        actionType.matchModelPath({ element: "Action1(EntityType2)" }),
+        false
+      );
+    });
+  });
+
+  describe("isBoundByType", function () {
+    it("bound with prefix with namespace", function () {
+      actionType.parameters[2].type = new EntityType({
+        $: { Name: "EntityType1" },
+      });
+      actionType.schema = {
+        namespace: "ns",
+      };
+      assert.equal(actionType.isBoundByType("ns.EntityType1"), true);
+    });
+    it("bound with prefix with alias", function () {
+      actionType.parameters[2].type = new EntityType({
+        $: { Name: "EntityType1" },
+      });
+      actionType.schema = {
+        alias: "ns",
+      };
+      assert.equal(actionType.isBoundByType("ns.EntityType1"), true);
+    });
+    it("invalid prefix", function () {
+      actionType.parameters[2].type = new EntityType({
+        $: { Name: "EntityType1" },
+      });
+      actionType.schema = {
+        alias: "SAP__self",
+      };
+      assert.equal(actionType.isBoundByType("ns.EntityType1"), false);
+    });
+    it("missing schema", function () {
+      actionType.parameters[2].type = new EntityType({
+        $: { Name: "EntityType1" },
+      });
+      assert.equal(actionType.isBoundByType("ns.EntityType1"), false);
+    });
   });
 });

af4f1e4d5ca47b7d5a52025a64767d9869f47ad9
Norbert Volf <norbert.volf@sap.com>
2026-01-13 11:33:44 +0100

    [FEATURE] Release version 2.6.0
    Topic: #0

--- @types/lib/model/oasis/schema/Action.d.ts
+++ @types/lib/model/oasis/schema/Action.d.ts
@@ -9,6 +9,24 @@ export = Action;
  */
 declare class Action extends BoundObject {
     static SCHEMA_GROUP: string;
+    /**
+     * Checks whether the action matches the given model path.
+     *
+     * @param {Object} parsedPath parsed model path
+     *
+     * @returns {Boolean} true if the action matches the model path
+     * @memberof Action
+     */
+    matchModelPath(parsedPath: any): boolean;
+    /**
+     * Checks whether the action is bound by the given type name.
+     *
+     * @param {String} boundTypeName bound type name to check
+     *
+     * @returns {Boolean} true if the action is bound by the given type
+     * @memberof Action
+     */
+    isBoundByType(boundTypeName: string): boolean;
 }
 import BoundObject = require("./BoundObject");
 //# sourceMappingURL=Action.d.ts.map
